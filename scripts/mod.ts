8dc23407526aaae1c5dc378b360d1f7552a87c5e
Olybear9 <olybear9@gmail.com>
2021-05-25 09:51:26 -0500

    🆕 feat(scripts): Update command

--- scripts/mod.ts
+++ scripts/mod.ts
@@ -8,10 +8,12 @@
 import { CommandHandler } from "./commands/Command.ts";
 import CountCommand from "./commands/CountCommand.ts";
 import HeaderCommand from "./commands/HeaderCommand.ts";
+import UpdateCommand from "./commands/UpdateCommand.ts";
 
 if (import.meta.main) {
 	const handler = new CommandHandler();
 	handler.register(new CountCommand());
 	handler.register(new HeaderCommand());
+	handler.register(new UpdateCommand());
 	handler.runMain();
 }
