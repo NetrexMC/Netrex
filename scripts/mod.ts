cb5d740bd9b3f0c5f41de29dc9104cff62086bd3
Olybear9 <olybear9@gmail.com>
2021-05-25 21:53:59 -0500

    ✏ script(main): Add drun to commands

--- scripts/mod.ts
+++ scripts/mod.ts
@@ -7,6 +7,7 @@
  */
 import { CommandHandler } from "./commands/Command.ts";
 import CountCommand from "./commands/CountCommand.ts";
+import DrunCommand from "./commands/DrunCommand.ts";
 import HeaderCommand from "./commands/HeaderCommand.ts";
 import UpdateCommand from "./commands/UpdateCommand.ts";
 import WorkInit from "./commands/WorkInitCommand.ts";
@@ -17,5 +18,6 @@ if (import.meta.main) {
 	handler.register(new HeaderCommand());
 	handler.register(new UpdateCommand());
 	handler.register(new WorkInit());
+	handler.register(new DrunCommand());
 	handler.runMain();
 }
