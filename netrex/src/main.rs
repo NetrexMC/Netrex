17b3230af7e32cacd5f276b10d9b7cec9fcd864c
Bavfalcon9 <olybear9@gmail.com>
2023-06-18 21:49:04 -0500

    lol

--- netrex/src/main.rs
+++ netrex/src/main.rs
@@ -1,3 +1,5 @@
+use rak_rs::server::Listener;
+
 mod network;
 mod player;
 mod plugin;
@@ -5,5 +7,13 @@ mod server;
 
 #[tokio::main]
 async fn main() {
+	// load network layer
+	let mut server = Listener::bind("0.0.0.0:19132").await.unwrap();
 
+	server.start().await.unwrap();
+
+	loop {
+		let conn = server.accept().await.unwrap();
+		println!("New connection from {}", conn.address);
+	}
 }
--- netrex/src/server/mod.rs
+++ netrex/src/server/mod.rs
@@ -1,4 +1,3 @@
-
 pub struct Server {
 
 }
