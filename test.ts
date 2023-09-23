2a72c54dc5576a9cb847e9169e5ea69595aa859e
john-bv <olybear9@gmail.com>
2023-09-22 21:14:31 -0500

    feat: add resources

---
+++ test.ts
@@ -0,0 +1,4 @@
+#!/bin/env/deno.exe
+console.log("%cERR!", "color: #FF0000; font-size: 2em;", "This is an error message");
+console.log('\nor\n')
+console.log("%c[ERR]", "color: #FF0000; font-size: 2em;", "This is an error message");
